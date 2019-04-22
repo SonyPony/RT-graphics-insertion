62afb04cb5acc1561934352e7ec8a1f7cf23ec19
Son Hai Nguyen <sirarseus@gmail.com>
2019-04-23 00:41:20 +0200

    REmove erosion unused functions

--- src/pipeline/morphology/erosion.h
+++ src/pipeline/morphology/erosion.h
@@ -1,4 +1,1 @@
-void NaiveErosion(int * src, int * dst, int width, int height, int radio);
-void ErosionTwoSteps(int * src, int * dst, int * temp, int width, int height, int radio);
-void ErosionTwoStepsShared(int * src, int * dst, int * temp, int width, int height, int radio);
 void ErosionTemplateSharedTwoSteps(uint8_t * src, uint8_t * temp, int width, int height, int radio);
